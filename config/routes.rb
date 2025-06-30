47a420ee9bcc513284603ddcfba551611cde3e37
kskisb <isbkosuke@gmail.com>
2025-06-30 13:14:13 +0900

    fix: rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -30,8 +30,8 @@ Rails.application.routes.draw do
         end
       end
 
-      resources :conversations, only: [:index, :show, :create] do
-        resources :messages, only: [:create]
+      resources :conversations, only: [ :index, :show, :create ] do
+        resources :messages, only: [ :create ]
       end
     end
   end
